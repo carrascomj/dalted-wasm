be999bc0ddeef569612332c9f076837a3bbea9df
carrascomj <carrascomurielj@gmail.com>
2023-10-01 13:53:39 +0200

    enhance: style HTML

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use js_sys::JsString;
 use leptos::*;
 use wasm_bindgen::{closure::Closure, JsCast};
-use web_sys::{Event, FileReader};
+use web_sys::{Event, FileReader, HtmlDivElement};
 mod image_processing;
 use image_processing::{color_filter, decode_raw_image, matrices::MATRICES};
 use serde::{Deserialize, Serialize};
@@ -15,23 +15,13 @@ pub struct Example(String);
 // 1) for mostly-static lists, using Rust iterators
 // 2) for lists that grow, shrink, or move items, using <For/>
 
+/// A list of images that will be filled with images.
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <h1>"Iteration"</h1>
-        <h2>"Static List"</h2>
-        <p>"Use this pattern if the list itself is static."</p>
-        <StaticList/>
-    }
-}
-
-/// A list of counters, without the ability
-/// to add or remove any.
-#[component]
-fn StaticList(cx: Scope) -> impl IntoView {
+    let (msg, set_msg) = create_signal(cx, "Send an Image!");
     let (shared_img, set_image): (ReadSignal<Option<image::DynamicImage>>, _) =
         create_signal(cx, None);
-    // create counter signals that start at incrementing numbers
+    // create the file input and file reader to use in the input button
     let file_input = create_node_ref::<html::Input>(cx);
     let filereader = FileReader::new().unwrap().dyn_into::<FileReader>().unwrap();
     let onload = Closure::wrap(Box::new(move |event: Event| {
@@ -45,45 +35,59 @@ fn StaticList(cx: Scope) -> impl IntoView {
     filereader.set_onloadend(Some(onload.as_ref().unchecked_ref()));
     onload.forget();
 
-    // when you have a list that doesn't change, you can
-    // manipulate it using ordinary Rust iterators
-    // and collect it into a Vec<_> to insert it into the DOM
+    // once shared_img changes, compute each image transformation
     let imgs = MATRICES.map(|mat| {
         view! { cx,
-            <li>
-                <picture>
-                <img src={move || shared_img.with(|im|
-                {
-                log!("Processing!");
-                String::from("data:image/png;base64,") + im.as_ref().ok_or(0)
-                    .map(|i| color_filter(i, mat).unwrap_or_default())
-                    .unwrap_or(String::from("")).as_str()
+                <picture class="flex-pic">
+                <img class="flex-img" src={move || shared_img.with(|im|
+                    {
+                    String::from("data:image/png;base64,") + im.as_ref().ok_or(0)
+                        .map(|i| color_filter(i, mat).unwrap_or_default())
+                        .unwrap_or(String::from("")).as_str()
                 })
                 }/>
                 </picture>
-            </li>
         }
     });
 
-    // Note that if `counter_buttons` were a reactive list
-    // and its value changed, this would be very inefficient:
-    // it would rerender every row every time the list changed.
+    // form to submit the image and img reactive frames
     view! { cx,
+
+        <div class="user-sweep" id="box">
+        <div class="upload-btn-wrapper">
+
+        <p>{msg}</p>
         <form>
             <input type="file" node_ref=file_input
                 on:change=move |ev| {
                     ev.prevent_default();
                     let files = file_input().unwrap().files().map(|x| x.get(0));
                     if let Some(Some(file)) = files {
+                         set_msg("Processing!");
                          filereader.read_as_binary_string(&file).unwrap();                    }
                     }
             />
-            <input type="submit"/>
+
         </form>
-        <ul>{imgs}</ul>
+        </div>
+        </div>
+        <div class="portrait">
+            {imgs}
+        </div>
     }
 }
 
 fn main() {
-    leptos::mount_to_body(|cx| view! { cx, <App/> })
+    leptos::mount_to(
+        web_sys::window()
+            .unwrap()
+            .document()
+            .unwrap()
+            .get_element_by_id("entrypoint")
+            .unwrap()
+            .dyn_into::<HtmlDivElement>()
+            .unwrap()
+            .into(),
+        |cx| view! { cx, <App/> },
+    );
 }
